ca53dbc566a34e2a670a2914636bbddc95fc5580
Mutum <victor.corin@hotmail.com>
2020-08-17 16:15:22 -0300

    just the basics with material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'curriculo';
+  curiosidade: boolean;
+  sobr: boolean = true;
+
+  curiosidades(){
+    this.curiosidade = true;
+    this.sobr = false;
+  }
+
+  sobre() {
+    this.curiosidade = false;
+    this.sobr = true;
+  }
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ComponentsModule } from 'src/app/components.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    ComponentsModule,
+    BrowserModule,
+    BrowserAnimationsModule
   ],
+  exports: [ComponentsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
